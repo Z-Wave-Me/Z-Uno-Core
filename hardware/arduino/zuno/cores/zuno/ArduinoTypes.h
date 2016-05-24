fa77fdd10b6ca4ec969093b1df4588622cb132c3
Alexander Polyakov <iu7-x3@mail.ru>
2016-05-24 18:10:20 +0300

    Небольшие фиксы для библиотеки Keypad. Чтобы она собиралась.

--- hardware/arduino/zuno/cores/zuno/ArduinoTypes.h
+++ hardware/arduino/zuno/cores/zuno/ArduinoTypes.h
@@ -1,5 +1,6 @@
 #ifndef ARDUINO_LIKE_TYPES__
 #define ARDUINO_LIKE_TYPES__
+#include "ZUNO_Definitions.h"
 
 typedef unsigned char uint8_t;
 typedef unsigned short size_t;
@@ -7,12 +8,28 @@ typedef unsigned char   BYTE;
 typedef unsigned short  WORD;
 typedef unsigned long   DWORD;
 
+//#define size_t  WORD
 #define byte 	BYTE
 #define word    WORD
 #define bool 	BYTE
+
+/*enum 
+{
+	FALSE  = 0,
+	TRUE   = 1
+};*/
+
+/*
+enum 
+{
+	false = 0,
+	true = 1
+}*/
 #define TRUE    1
 #define FALSE   0
 
+//#define true 	TRUE
+//#define false	FALSE
 
 
 #endif // ARDUINO_LIKE_TYPES__
