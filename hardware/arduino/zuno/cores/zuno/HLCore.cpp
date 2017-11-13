819553aede4d0d92869251e6a3eeae3af99bd1ff
Alexander Polyakov <iu7-x3@mail.ru>
2017-11-13 23:46:04 +0300

    The revision was updated.

--- hardware/arduino/zuno/cores/zuno/HLCore.cpp
+++ hardware/arduino/zuno/cores/zuno/HLCore.cpp
@@ -151,6 +151,7 @@ void delay(dword value) {
 	return;
 }
 /// math
+
 long map(long x, long in_min, long in_max, long out_min, long out_max)
 {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
